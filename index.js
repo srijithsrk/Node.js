8a9bdac66a7a1347bcd32eaf229a593cec724cce
Srijith <30682653+srijithsrk@users.noreply.github.com>
2019-03-24 23:43:39 +0530

    updated

--- index.js
+++ index.js
@@ -11,3 +11,32 @@ res.send('HI')
 })
 
 app.listen(3000)       // connect to server on port 3000
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
+
+
+const express = require('express')
+const path = require('path')
+const hoganMiddleware = require('hogan-middleware')
+
+const app = express()
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'mustache')
+app.engine('mustache', hoganMiddleware.__express)
+
+app.get('/', (req, res, next) => {
+	res.send('Hello!!')
+})
+
+app.get('/json', (req, res, next) => {
+	const data = {
+		greeting: 'Hello!!'
+	}
+
+	res.json(data)
+})
+
+app.get('/home', (req, res, next) => {
+	res.render('home', null)
+})
+
+app.listen(3000) // 3000, 5000, 8000, 8080
